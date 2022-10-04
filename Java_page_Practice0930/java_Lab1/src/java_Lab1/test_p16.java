43b00e5a53a54da94527fc6395a132565f702bb4
ting-weii <ptsoho.tine@gmail.com>
2022-10-04 11:36:31 +0800

    Java_Practice0930

---
+++ Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p16.java
@@ -0,0 +1,11 @@
+package java_Lab1;
+//109360127_電子三甲_許廷維
+public class test_p16 
+{
+	public static void main(String[] args)
+	{
+		System.out.println('A');
+		System.out.println("歡迎使用Java!");
+		System.out.println(123);
+	}
+}
---
+++ Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p38.java
@@ -0,0 +1,16 @@
+package java_Lab1;
+//109360127_電子三甲_許廷維
+public class test_p38 
+{
+	public static void main(String[] args)
+	{
+		double dnum = 160.5;
+		System.out.println("身高是"+dnum+"公分");
+		
+		System.out.println("指定給int型態的變數");
+		
+		int inum = (int)dnum;
+		System.out.println("身高是"+inum+"公分");
+	}
+
+}
