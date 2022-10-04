82c7459c41f1c453d56fa90d2f5a021f1622a450
ting-weii <ptsoho.tine@gmail.com>
2022-10-04 14:03:09 +0800

    Java_practice0930new

--- Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p09.java
+++ Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p09.java
@@ -4,7 +4,7 @@ public class test_p09
 {
 	public static void main(String[] args)
 	{
-		System.out.println("歡迎使用Java!");
-		System.out.println("開始使用Java吧!");
+		System.out.print("歡迎使用Java!");
+		System.out.print("開始使用Java吧!");
 	}
 }
--- Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p10.java
+++ Java_page_Practice0930/java_Lab1/src/java_Lab1/test_p10.java
@@ -1,6 +1,10 @@
 package java_Lab1;
 //109360127_電子三甲_許廷維
-public class test_p10 
+public class test_p10
 {
-
+	public static void main(String[] args)
+	{
+		System.out.println("歡迎使用Java!");
+		System.out.println("開始使用Java吧!");
+	}
 }
